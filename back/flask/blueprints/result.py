5de5aaa6c2ce9451cc24e6402d34a7b5b1d823b4
xzy12139 <96683999+xzy12139@users.noreply.github.com>
2022-12-03 19:56:10 +0800

    Update result.py

--- back/flask/blueprints/result.py
+++ back/flask/blueprints/result.py
@@ -1,9 +1,21 @@
 # 和结果展示与分析有关的蓝图页
 from flask import Blueprint
+from models import Thesis
+from util import Result
+from exts import db
 
-bp = Blueprint("auth", __name__, url_prefix="/result")
+bp = Blueprint("result", __name__, url_prefix="/result")
 
 
 @bp.route("single")
 def single():
-    pass
+    query = {}
+    query = db.session.query(Thesis.thesis_id, Thesis.title, Thesis.author, Thesis.publication_date, Thesis.journal, Thesis.abstract, Thesis.link, Thesis.citation_num, Thesis. rating).order_by(Thesis.thesis_id.asc()).all()
+    res = []
+    content = {}
+    for i in query:
+        content = {'thesis_id': i[0], 'title': i[1], 'author': i[2], 'publication_date': i[3], 'journal': i[4], 'abstract': i[5], 'link': i[6], 'citation_num': i[7], 'rating': i[8]}
+        res.append(content)
+        content = {}
+    print (res)
+    return Result.success(res)
