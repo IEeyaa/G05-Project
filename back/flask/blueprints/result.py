2114fbd9e3bc20e79a0efe78c1f736ad83363d32
IEeyaa <105735506+IEeyaa@users.noreply.github.com>
2022-12-10 00:02:27 +0800

    1210 把排序移到前端，后端改成收藏处理

--- back/flask/blueprints/result.py
+++ back/flask/blueprints/result.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from models import Thesis
+from models import Thesis, Favorites
 from util import Result
 from exts import db
 
@@ -20,6 +20,21 @@ def infor_data():
     return Result.success(alldata)
 
 
+@bp.route("CompareView", methods=['POST'])
+def comp_data():
+    data = request.get_json()
+    if not data:
+        return Result.error(400, 'post 必须是json数据')
+    thesis_id = data.get('thesis_id', None)
+    print(thesis_id)
+    all_thesis = Thesis.query.filter_by(thesis_id=thesis_id).all()
+    alldata = []
+    for single_thesis in all_thesis:
+        single_data = Thesis.to_dict(single_thesis)
+        alldata.append(single_data)
+    return Result.success(alldata)
+
+
 @bp.route("HomeView", methods=['GET', 'POST'])
 def home_data():
     # get请求，用于初始化
@@ -30,25 +45,26 @@ def home_data():
             single_data = Thesis.to_dict(single_thesis)
             alldata.append(single_data)
         return Result.success(alldata)
-    # post请求，用于排序后展示
+    # post请求，用于处理用户收藏
     else:
         data = request.get_json()
         if not data:
             return Result.error(400, 'post 必须是json数据')
-        rule = int(data.get('rule', None))
-        # 排序方式
-        if rule == 1:
-            all_thesis = db.session.query(Thesis).order_by(Thesis.thesis_id).all()
-        elif rule == 2:
-            all_thesis = db.session.query(Thesis).order_by(Thesis.title).all()
-        elif rule == 3:
-            all_thesis = db.session.query(Thesis).order_by(Thesis.publication_date.desc()).all()
-        else:
-            all_thesis = db.session.query(Thesis).order_by(Thesis.citation_num).all()
 
-        alldata = []
-        for single_thesis in all_thesis:
-            single_data = Thesis.to_dict(single_thesis)
-            alldata.append(single_data)
-        return Result.success(alldata)
+        thesis_id = int(data.get('thesis_id', None))
+        user_id = int(data.get('user_id', None))
+        if user_id == -1:
+            message = "用户未登录"
+            return Result.error(400, message)
+
+        has_contain = Favorites.query.filter_by(thesis_id=thesis_id, user_id=user_id).first()
+
+        if has_contain:
+            message = "该论文已收藏"
+            return Result.error(400, message)
+
+        favor = Favorites(thesis_id=thesis_id, user_id=user_id)
+        db.session.add(favor)
+        db.session.commit()
+        return Result.success("OK")
 
